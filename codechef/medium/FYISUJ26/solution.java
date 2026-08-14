6ba6e220bc571f2113d015ab600caad06a091ecc
Arvind Kumar <contact@aarvind.dev>
2026-08-14 12:06:02 +0530

    ✅ FYISUJ26 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/FYISUJ26/solution.java
@@ -0,0 +1,9 @@
+ class Codechef{
+    public static void main(String[] args){
+        long transactionID = 512345678900l;
+        short branchCode = 3052;
+        
+    System.out.println("Tranaction ID: "+ transactionID);
+    System.out.print("Branch Code"+ branchCode);
+    }
+}
