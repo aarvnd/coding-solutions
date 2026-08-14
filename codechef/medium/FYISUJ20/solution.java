29a2219f18bece27d04c38cce5eb4dd0ca0ca409
Arvind Kumar <contact@aarvind.dev>
2026-08-14 11:56:25 +0530

    ✅ FYISUJ20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/FYISUJ20/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign population values
+
+        int cityPopulation = 5000000;
+        Short districtPopulation = 25000;
+
+        // Print the population values
+
+        System.out.println("City population: "+ cityPopulation);
+        System.out.println("District Population: "+ districtPopulation);
+    }
+}
