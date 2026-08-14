7b17573e981d7feb8dc557d97cc42ab7cea43f36
Arvind Kumar <contact@aarvind.dev>
2026-08-14 11:26:20 +0530

    ✅ OBZPOG25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/OBZPOG25/solution.java
@@ -0,0 +1,5 @@
+class Codechef {
+    public static void main(String[] args) {
+        System.out.print("The programmer exclaimed - \"It's amazing how Python handles special characters!\"");
+    }
+}
