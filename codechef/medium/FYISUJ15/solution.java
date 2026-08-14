6f00a978c9fa1209b2ff39753fb5412d364f8ca9
Arvind Kumar <contact@aarvind.dev>
2026-08-14 11:49:56 +0530

    ✅ FYISUJ15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/FYISUJ15/solution.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main(String[] args) {
+        //  Declare and assign earthquake intensity readings
+
+        short regionIntensity = 12000;
+        short regionTwoIntensity = -5000;
+        byte regionThreeIntensity = 0;
+
+
+        //  Print the intensity values
+
+        System.out.println("Region one Intensity: "+ regionIntensity);
+        System.out.println("Region Two Intensity: "+ regionTwoIntensity);
+        System.out.println("Region Three Intensity: "+ regionThreeIntensity);
+
+    }
+}
